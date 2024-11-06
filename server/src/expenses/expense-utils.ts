7fe3138826829eaef12d10819e0f8722d6a0b160
risabsank <risab.sankar@gmail.com>
2024-11-06 11:06:32 -0800

    Exercise 2 done.

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -7,35 +7,40 @@ export async function createExpenseServer(req: Request, res: Response, db: Datab
     try {
         // Type casting the request body to the expected format.
         const { id, cost, description } = req.body as { id: string, cost: number, description: string };
- 
+
         if (!description || !id || !cost) {
             return res.status(400).send({ error: "Missing required fields" });
         }
- 
+
         await db.run('INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);', [id, description, cost]);
         res.status(201).send({ id, description, cost });
- 
+
     } catch (error) {
- 
+
         return res.status(400).send({ error: `Expense could not be created, + ${error}` });
     };
- }
+}
 
-export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
+export async function deleteExpense(req: Request, res: Response, db: Database) {
     const { id } = req.params;
 
     if (!id) {
-        return res.status(400).send({ error: "ID not found: ${id}" });
+        return res.status(400).send({ error: `ID not found: ${id}` });
     }
 
-    const expenseIndex = expenses.findIndex(expense => expense.id === id);
-    if (expenseIndex === -1) {
-        return res.status(404).send({ error: "Expense not found" });
+    try {
+        const expense = await db.get('SELECT * FROM expenses WHERE id = ?', [id]);
+
+        if (!expense) {
+            return res.status(404).send({ error: "Expense not found" });
+        }
+
+        await db.run('DELETE FROM expenses WHERE id = ?', [id]);
+
+        res.status(200).send({ message: "Expense deleted" });
+    } catch (error) {
+        return res.status(400).send({ error: "Could not delete expense" });
     }
-
-    expenses.splice(expenseIndex, 1);
-
-    res.status(200).send({ message: "Expense deleted" });
 }
 
 export async function getExpenses(req: Request, res: Response, db: Database) {
